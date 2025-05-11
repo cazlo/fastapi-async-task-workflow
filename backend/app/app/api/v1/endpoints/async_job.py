e0d57b380d6a5e895951f36bab2340ec4c27f460
Drew Paettie <cazlo@users.noreply.github.com>
2025-05-11 11:54:36 -0700

    basic revoke behavior for async_job

--- backend/app/app/api/v1/endpoints/async_job.py
+++ backend/app/app/api/v1/endpoints/async_job.py
@@ -1,13 +1,15 @@
 from fastapi import APIRouter
 from app.api.celery_task import increment
 from celery.result import AsyncResult
+from celery.states import REVOKED
 from pydantic import BaseModel
 
 router = APIRouter()
 
 
 class TaskInput(BaseModel):
-    delay: int
+    delay: int         # how much time the task sleeps for before doing the increment
+    countdown: int = 0 # delay before the task can be started
 
 
 class TaskResponse(BaseModel):
@@ -17,24 +19,38 @@ class TaskResponse(BaseModel):
     result: int | None
 
 
-@router.post("/increment_task", response_model=TaskResponse)
+@router.post("/increment_task", response_model=TaskResponse, status_code=201)
 async def queue_increment_task(input_data: TaskInput):
-    task_result = increment.apply_async(args=(input_data.delay,))
+    task_result = increment.apply_async(args=(input_data.delay,), countdown=input_data.countdown)
+    ready = task_result.ready()
     return TaskResponse(
         task_id=task_result.id,
-        status="started",
-        ready=task_result.ready(),
-        result=task_result.result if task_result.ready() else None,
+        status="QUEUED",
+        ready=ready,
+        result=task_result.result if ready else None,
     )
 
 
 @router.get("/increment_task/{task_id}", response_model=TaskResponse)
 async def get_increment_task_result(task_id: str):
     task_result = AsyncResult(task_id)
-    print(task_result)
-    return {
-        "task_id": task_id,
-        "status": task_result.status,
-        "ready": task_result.ready(),
-        "result": task_result.result if task_result.ready() else None,
-    }
+    ready = task_result.ready()
+    return TaskResponse(
+        task_id=task_result.id,
+        status=task_result.status,
+        ready=ready,
+        result=task_result.result if ready and not task_result.status == REVOKED else None,
+    )
+
+@router.delete("/increment_task/{task_id}", response_model=TaskResponse)
+async def get_increment_task_result(task_id: str):
+    task_result = AsyncResult(task_id)
+    task_result.revoke() # this will remove it from queue but not stop the job from processing if it has already started
+    task_result = AsyncResult(task_id)
+    ready = task_result.ready()
+    return TaskResponse(
+        task_id=task_result.id,
+        status=task_result.status,
+        ready=task_result.ready(),
+        result=task_result.result if ready and not task_result.status == REVOKED else None,
+    )
--- backend/app/test/api/test_async_job.py
+++ backend/app/test/api/test_async_job.py
@@ -2,25 +2,49 @@ import time
 
 import pytest
 from fastapi.testclient import TestClient
-from typing import AsyncGenerator
 
 from app.api.v1.endpoints.async_job import TaskResponse
 from app.main import app
-from app.core.config import settings
 
 url = "http://fastapi.localhost:1080/api/v1"
 test_client = TestClient(app, base_url=url)
 
-def test(celery_worker):
-    input = 1
-    response = TaskResponse(**test_client.post("async_job/increment_task", json={
-      "delay": input
-    }).json())
-    id = response.task_id
+def test_queue_and_finish_increment_task(celery_worker):
+    test_input = 1
+    response = test_client.post("async_job/increment_task", json={
+      "delay": test_input
+    })
+    assert response.status_code == 201
+    response_body = TaskResponse(**response.json())
+    task_id = response_body.task_id
     for tries in range(9):
-        response = TaskResponse(**test_client.get(f"async_job/increment_task/{id}").json())
-        if response.ready:
-            assert response.result == input + 1
+        response = test_client.get(f"async_job/increment_task/{task_id}")
+        assert response.status_code == 200
+        response_body = TaskResponse(**response.json())
+        if response_body.ready:
+            assert response_body.result == test_input + 1
         else:
             time.sleep(0.5)
 
+
+@pytest.mark.skip(reason="Only working for situation where we don't use the embedded celery_worker")
+def test_delete_task(celery_worker):
+    test_input = 1
+    response = test_client.post("async_job/increment_task", json={
+      "delay": test_input,
+      "countdown": 10
+    })
+    assert response.status_code == 201
+    response_body = TaskResponse(**response.json())
+    task_id = response_body.task_id
+
+    delete_response = test_client.delete(f"async_job/increment_task/{task_id}")
+    assert delete_response.status_code == 200
+    delete_response_body = TaskResponse(**delete_response.json())
+    assert delete_response_body.ready == False
+    assert delete_response_body.status == "PENDING"
+
+    response = test_client.get(f"async_job/increment_task/{task_id}")
+    assert response.status_code == 200
+    response_body = TaskResponse(**response.json())
+    assert response_body.status == "REVOKED"
